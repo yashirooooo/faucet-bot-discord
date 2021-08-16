703c2c96b9f8334081b27bb8d3aa8d2c31cc1a17
Yashiro <1161569428@qq.com>
2021-08-16 11:29:06 +0800

    Update to transfer

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,50 @@
 import { logger } from '@polkadot/util';
 import pRetry from 'p-retry';
-import { token } from './consts';
+import { token, chainAddr, trasferAmount, seeds } from './consts';
 const { Client, Intents } = require('discord.js');
+import { Keyring } from '@polkadot/keyring';
+import { sendCru } from './crustApi';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { typesBundleForPolkadot } from '@crustio/type-definitions';
+
+const keyring = new Keyring();
+
+const isValidAddr = (addr: string) => {
+    try {
+      keyring.decodeAddress(addr);
+      return true;
+    } catch (error) {
+      return false;
+    }
+}
 
 const l = logger('main');
 
 const bot = () => {
-    console.log('token', token)
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], restRequestTimeout: 60 * 6000 });
-
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
-    
-    client.on('error', console.error);
-    
-    client.on('message', (msg: { content: string; reply: (arg0: string) => void; }) => {
-        if (msg.content === 'hi') {
-            msg.reply('hello');
-        }
-    });
-
-    client.login(token);
+    const provider = new WsProvider(chainAddr);
+    ApiPromise.create({
+      provider,
+      typesBundle: typesBundleForPolkadot,
+    }).then(async (api) => {
+      const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], restRequestTimeout: 60 * 6000 });
+      client.on('ready', () => {
+          console.log(`Logged in as ${client.user.tag}!`);
+      });
+      
+      client.on('error', console.error);
+      
+      client.on('message', async (msg: { content: string; reply: (arg0: string) => void; }) => {
+          const address = msg.content;
+          if (isValidAddr(address)) {
+            const result = await sendCru(api, address, seeds);
+            msg.reply(result.details);
+          }
+      });
+  
+      client.login(token);
+    }).catch(async (e) => {
+      process.exit(1);
+    })   
 }
 
 // TODO: add error handling
